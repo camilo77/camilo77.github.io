70d203c7e3a99b26d653e4d0a8e17ada7365a738
camilo77 <camilopineda77@hotmail.com>
2014-09-16 23:12:48 -0500

    prueba

---
+++ prueba/funciones.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+	$("div").click(function(){
+
+		/*posicion de un elemento
+		$(this).position().top*/
+		/*var pos = $(this).position().top*/
+		/*posicionar el scroll de la pagina en la posicion indicada*/
+		/*$(window).scrollTop(pos)*/
+
+		
+		
+		focoElemento(this,0,900);
+	});
+	$( "#clickme" ).click(function() {
+	  $( "#book" ).animate({
+	    opacity: 0.25,
+	    left: "+=50",
+	    top:"-=500"
+	  }, 500);
+	});
+});
+
+/*  
+	enfocar elemento en la posicion deseada
+	focoElemento(this,0,900);
+*/
+function focoElemento(elemento, top, velocidad){
+
+	/*encontrando la posicion del elemento a enfocar*/
+	var posicion = $(elemento).position().top
+
+	/*creando efecto y enfocando*/
+	$('html, body').animate(
+		{scrollTop: posicion-top},velocidad
+	);
+}
+
+/*
+	enfocar pagina en la posicion deseada
+	focoPagina(100,400)
+*/
+function focoPagina(posicion, velocidad){
+	/*enfocando pagina*/
+	$('html, body').animate(
+		{scrollTop: posicion},velocidad
+	);
+}
+
+
+/*saber posicion de el scroll de la pagina
+			
+			$(window).scrollTop()
+			$(window).scrollLeft()
+
+			retorna valor de pixeles
+*/
+
+/*mover el scroll de la ventana a la posicion deseada
+
+	$(window).scrollTop(pos)
+
+*/
+
+/*conocer posiscion de un elemento
+	
+	$(elemento).position()
+	$(elemento).position().top
+	$(elemento).position().left
+*/
